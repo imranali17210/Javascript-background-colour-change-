e115147b5f7a59bdd9787c3bd77d0020ca943c88
Imran Ali <110028564+imranali17210@users.noreply.github.com>
2022-08-28 17:18:33 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+const button = document.querySelector('button')
+const body = document.querySelector('body')
+const color = ['red','pink','black','blue','gray','green']
+body.style.backgroundColor = 'violet'
+button.addEventListener('click',changeB)
+
+function changeB() {
+  const colorIndex = parseInt(Math.random()*color.length)
+  body.style.backgroundColor = color[colorIndex]
+}
